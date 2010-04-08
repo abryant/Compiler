ca73e7c01b739f0c9eb067955a94326671f72b74
Anthony Bryant <antjbryant@gmail.com>
2010-04-08 19:54:18 +0000

    Improve token storage and generation in the test expression tokenizer.
    git-svn-id: file:///srv/svn/repo/trunk/Programs/Compiler@304 c9e57fc7-f7bf-4014-a52f-2ff4955da382

--- src/test/parser/ExpressionTokenizer.java
+++ src/test/parser/ExpressionTokenizer.java
@@ -16,69 +16,47 @@ public class ExpressionTokenizer extends Tokenizer
   
   private int state = 0;
   
+  private static final Token[] TEST1 =
+  {
+    new Token(ExpressionType.NUMBER, new Number(1)),
+    new Token(ExpressionType.TIMES, ExpressionType.TIMES),
+    new Token(ExpressionType.NUMBER, new Number(2)),
+    new Token(ExpressionType.PLUS, ExpressionType.PLUS),
+    new Token(ExpressionType.NUMBER, new Number(3)),
+    new Token(ExpressionType.TIMES, ExpressionType.TIMES),
+    new Token(ExpressionType.LPAREN, ExpressionType.LPAREN),
+    new Token(ExpressionType.NUMBER, new Number(12)),
+    new Token(ExpressionType.MINUS, ExpressionType.MINUS),
+    new Token(ExpressionType.NUMBER, new Number(6)),
+    new Token(ExpressionType.RPAREN, ExpressionType.RPAREN)
+  };
+  
+  private static final Token[] TEST2 =
+  {
+    new Token(ExpressionType.LPAREN, ExpressionType.LPAREN),
+    new Token(ExpressionType.NUMBER, new Number(6)),
+    new Token(ExpressionType.TIMES, ExpressionType.TIMES),
+    new Token(ExpressionType.NUMBER, new Number(2)),
+    new Token(ExpressionType.RPAREN, ExpressionType.RPAREN)
+  };
+  
+  private Token[] tokens = TEST2;
+  
+  
+  
   /**
    * @see compiler.parser.Tokenizer#generateToken()
    */
   @Override
   protected Token generateToken()
   {
-    state++;
-    /*
-    switch (state)
+    if (state < tokens.length)
     {
-    case 1:
-      return new Token(ExpressionType.NUMBER, new Number(1));
-    case 2:
-      return new Token(ExpressionType.TIMES, null);
-    case 3:
-      return new Token(ExpressionType.NUMBER, new Number(2));
-    case 4:
-      return new Token(ExpressionType.PLUS, null);
-    case 5:
-      return new Token(ExpressionType.NUMBER, new Number(3));
-    case 6:
-      return new Token(ExpressionType.LPAREN, null);
-    case 7:
-      return new Token(ExpressionType.NUMBER, new Number(5));
-    case 8:
-      return new Token(ExpressionType.PLUS, null);
-    case 9:
-      return new Token(ExpressionType.NUMBER, new Number(6));
-    case 10:
-      return new Token(ExpressionType.RPAREN, null);
-    default:
-      return null;
-    }
-    */
-    
-    switch (state)
-    {
-    case 1:
-      return new Token(ExpressionType.NUMBER, new Number(1));
-    case 2:
-      return new Token(ExpressionType.TIMES, ExpressionType.TIMES);
-    case 3:
-      return new Token(ExpressionType.NUMBER, new Number(2));
-    case 4:
-      return new Token(ExpressionType.PLUS, ExpressionType.PLUS);
-    case 5:
-      return new Token(ExpressionType.NUMBER, new Number(3));
-    case 6:
-      return new Token(ExpressionType.TIMES, ExpressionType.TIMES);
-    case 7:
-      return new Token(ExpressionType.LPAREN, ExpressionType.LPAREN);
-    case 8:
-      return new Token(ExpressionType.NUMBER, new Number(12));
-    case 9:
-      return new Token(ExpressionType.MINUS, ExpressionType.MINUS);
-    case 10:
-      return new Token(ExpressionType.NUMBER, new Number(6));
-    case 11:
-      return new Token(ExpressionType.RPAREN, ExpressionType.RPAREN);
-    default:
-      return null;
+      state++;
+      return tokens[state - 1];
     }
     
+    return null;
   }
   
 }
