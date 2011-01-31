3a6a99eeef5fc12ff46cbc9352a5e02e2b810e5d
Anthony Bryant <antjbryant@gmail.com>
2011-01-31 03:20:36 +0000

    Remove the "static" keyword on import declarations, as it is redundant information.
    git-svn-id: file:///srv/svn/repo/trunk/Programs/Compiler@456 c9e57fc7-f7bf-4014-a52f-2ff4955da382

--- src/compiler/language/ast/topLevel/ImportDeclarationAST.java
+++ src/compiler/language/ast/topLevel/ImportDeclarationAST.java
@@ -17,21 +17,18 @@ public class ImportDeclarationAST
 
   private QNameAST name;
   private boolean all;
-  private boolean staticImport;
 
   /**
    * Creates a new ImportDeclarationAST with the specified name, and whether or not to import all names under this name.
    * @param name - the name to import
    * @param all - true to import all names under this one, false otherwise
-   * @param staticImport - true if this should represent a static import, false otherwise
    * @param parseInfo - the parsing information
    */
-  public ImportDeclarationAST(QNameAST name, boolean all, boolean staticImport, ParseInfo parseInfo)
+  public ImportDeclarationAST(QNameAST name, boolean all, ParseInfo parseInfo)
   {
     this.parseInfo = parseInfo;
     this.name = name;
     this.all = all;
-    this.staticImport = staticImport;
   }
 
   /**
@@ -51,14 +48,6 @@ public class ImportDeclarationAST
   }
 
   /**
-   * @return true if this ImportDeclarationAST represents a static import, false otherwise
-   */
-  public boolean isStaticImport()
-  {
-    return staticImport;
-  }
-
-  /**
    * @return the parseInfo
    */
   public ParseInfo getParseInfo()
@@ -74,10 +63,6 @@ public class ImportDeclarationAST
   {
     StringBuffer buffer = new StringBuffer();
     buffer.append("import ");
-    if (staticImport)
-    {
-      buffer.append("static ");
-    }
     buffer.append(name);
     if (all)
     {
--- src/compiler/language/parser/rules/topLevel/ImportDeclarationRule.java
+++ src/compiler/language/parser/rules/topLevel/ImportDeclarationRule.java
@@ -6,7 +6,6 @@ import static compiler.language.parser.ParseType.IMPORT_KEYWORD;
 import static compiler.language.parser.ParseType.QNAME;
 import static compiler.language.parser.ParseType.SEMICOLON;
 import static compiler.language.parser.ParseType.STAR;
-import static compiler.language.parser.ParseType.STATIC_KEYWORD;
 import parser.ParseException;
 import parser.Production;
 import parser.Rule;
@@ -25,17 +24,15 @@ import compiler.language.parser.ParseType;
  */
 public final class ImportDeclarationRule extends Rule<ParseType>
 {
-  private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 2L;
 
   private static final Production<ParseType> NORMAL_PRODUCTION = new Production<ParseType>(IMPORT_KEYWORD, QNAME, SEMICOLON);
   private static final Production<ParseType> ALL_PRODUCTION = new Production<ParseType>(IMPORT_KEYWORD, QNAME, DOT, STAR, SEMICOLON);
-  private static final Production<ParseType> STATIC_NORMAL_PRODUCTION = new Production<ParseType>(IMPORT_KEYWORD, STATIC_KEYWORD, QNAME, SEMICOLON);
-  private static final Production<ParseType> STATIC_ALL_PRODUCTION = new Production<ParseType>(IMPORT_KEYWORD, STATIC_KEYWORD, QNAME, DOT, STAR, SEMICOLON);
 
   @SuppressWarnings("unchecked")
   public ImportDeclarationRule()
   {
-    super(IMPORT_DECLARATION, NORMAL_PRODUCTION, ALL_PRODUCTION, STATIC_NORMAL_PRODUCTION, STATIC_ALL_PRODUCTION);
+    super(IMPORT_DECLARATION, NORMAL_PRODUCTION, ALL_PRODUCTION);
   }
 
   /**
@@ -47,22 +44,12 @@ public final class ImportDeclarationRule extends Rule<ParseType>
     if (NORMAL_PRODUCTION.equals(production))
     {
       QNameAST qname = (QNameAST) args[1];
-      return new ImportDeclarationAST(qname, false, false, ParseInfo.combine((ParseInfo) args[0], qname.getParseInfo(), (ParseInfo) args[2]));
+      return new ImportDeclarationAST(qname, false, ParseInfo.combine((ParseInfo) args[0], qname.getParseInfo(), (ParseInfo) args[2]));
     }
     if (ALL_PRODUCTION.equals(production))
     {
       QNameAST qname = (QNameAST) args[1];
-      return new ImportDeclarationAST(qname, true, false, ParseInfo.combine((ParseInfo) args[0], qname.getParseInfo(), (ParseInfo) args[2], (ParseInfo) args[3], (ParseInfo) args[4]));
-    }
-    if (STATIC_NORMAL_PRODUCTION.equals(production))
-    {
-      QNameAST qname = (QNameAST) args[2];
-      return new ImportDeclarationAST(qname, false, true, ParseInfo.combine((ParseInfo) args[0], (ParseInfo) args[1], qname.getParseInfo(), (ParseInfo) args[3]));
-    }
-    if (STATIC_ALL_PRODUCTION.equals(production))
-    {
-      QNameAST qname = (QNameAST) args[2];
-      return new ImportDeclarationAST(qname, true, true, ParseInfo.combine((ParseInfo) args[0], (ParseInfo) args[1], qname.getParseInfo(), (ParseInfo) args[3], (ParseInfo) args[4], (ParseInfo) args[5]));
+      return new ImportDeclarationAST(qname, true, ParseInfo.combine((ParseInfo) args[0], qname.getParseInfo(), (ParseInfo) args[2], (ParseInfo) args[3], (ParseInfo) args[4]));
     }
     throw badTypeList();
   }
