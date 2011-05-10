4b20d09f40f39721f77e2db3ec7980a8926f4680
Anthony Bryant <antjbryant@gmail.com>
2011-05-10 01:38:04 +0100

    Improve error reporting by printing out ParseInfo

--- src/test/language/ConceptualTranslationTest.java
+++ src/test/language/ConceptualTranslationTest.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.util.LinkedList;
 import java.util.List;
 
+import compiler.language.ast.ParseInfo;
 import compiler.language.conceptual.ConceptualException;
 import compiler.language.conceptual.NameConflictException;
 import compiler.language.conceptual.topLevel.ConceptualFile;
@@ -21,7 +22,7 @@ import compiler.language.translator.conceptual.ConceptualTranslator;
 public class ConceptualTranslationTest
 {
 
-  public static void main(String[] args) throws NameConflictException, ConceptualException
+  public static void main(String[] args)
   {
     if (args.length < 2)
     {
@@ -49,7 +50,20 @@ public class ConceptualTranslationTest
       System.out.println("Successfully parsed " + file.getAbsolutePath());
       System.out.println(conceptualFile);
 
-      translator.translate();
+      try
+      {
+        translator.translate();
+      }
+      catch (NameConflictException e)
+      {
+        printConceptualException(e.getMessage(), e.getParseInfo());
+        return;
+      }
+      catch (ConceptualException e)
+      {
+        printConceptualException(e.getMessage(), e.getParseInfo());
+        return;
+      }
 
       System.out.println("Successfully translated");
       System.out.println("Classes: ");
@@ -59,6 +73,53 @@ public class ConceptualTranslationTest
         System.out.println("  extends: " + conceptualClass.getBaseClass().getTypeInstance());
       }
 
+    }
+  }
+
+  private static void printConceptualException(String message, ParseInfo... parseInfos)
+  {
+    if (parseInfos == null || parseInfos.length < 1)
+    {
+      System.err.println(message);
+      return;
+    }
+    // make a String representation of the ParseInfos' character ranges
+    StringBuffer buffer = new StringBuffer();
+    for (int i = 0; i < parseInfos.length; i++)
+    {
+      String characterRange;
+      int startLine = parseInfos[i].getStartLine();
+      int endLine = parseInfos[i].getEndLine();
+      if (startLine == endLine)
+      {
+        // line:start-end if it is all on one line
+        characterRange = startLine + ":";
+        int startPos = parseInfos[i].getStartPos();
+        int endPos = parseInfos[i].getEndPos();
+        characterRange += startPos;
+        if (startPos < endPos - 1)
+        {
+          characterRange += "-" + (endPos - 1);
+        }
+      }
+      else
+      {
+        // startLine-endLine if it spans multiple lines
+        characterRange = startLine + "-" + endLine;
+      }
+      buffer.append(characterRange);
+      if (i != parseInfos.length - 1)
+      {
+        buffer.append(", ");
+      }
+    }
+    if (parseInfos.length == 1)
+    {
+      System.err.println(message + ": " + buffer);
+    }
+    else
+    {
+      System.err.println(message + "\nAt: " + buffer);
     }
   }
 
