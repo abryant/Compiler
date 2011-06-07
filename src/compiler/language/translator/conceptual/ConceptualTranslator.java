f9f92800d500b5cdd9af14b4b05869a4ab34dccb
Anthony Bryant <antjbryant@gmail.com>
2011-06-07 01:55:26 +0100

    Improve conceptual exception reporting

--- src/compiler/language/translator/conceptual/ConceptualTranslator.java
+++ src/compiler/language/translator/conceptual/ConceptualTranslator.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.List;
 
+import compiler.language.ast.ParseInfo;
 import compiler.language.ast.topLevel.CompilationUnitAST;
 import compiler.language.conceptual.ConceptualException;
 import compiler.language.conceptual.NameConflictException;
@@ -97,13 +98,60 @@ public class ConceptualTranslator
     }
     catch (ConceptualException e)
     {
-      e.printStackTrace();
+      printConceptualException(e.getMessage(), e.getParseInfo());
       return null;
     }
     catch (NameConflictException e)
     {
-      e.printStackTrace();
+      printConceptualException(e.getMessage() == null ? "Name conflict detected" : e.getMessage(), e.getParseInfo());
       return null;
+    }
+  }
+
+  private static void printConceptualException(String message, ParseInfo... parseInfos)
+  {
+    if (parseInfos == null || parseInfos.length < 1)
+    {
+      System.err.println(message);
+      return;
+    }
+    // make a String representation of the ParseInfos' character ranges
+    StringBuffer buffer = new StringBuffer();
+    for (int i = 0; i < parseInfos.length; i++)
+    {
+      String characterRange;
+      int startLine = parseInfos[i].getStartLine();
+      int endLine = parseInfos[i].getEndLine();
+      if (startLine == endLine)
+      {
+        // line:start-end if it is all on one line
+        characterRange = startLine + ":";
+        int startPos = parseInfos[i].getStartPos();
+        int endPos = parseInfos[i].getEndPos();
+        characterRange += startPos;
+        if (startPos < endPos - 1)
+        {
+          characterRange += "-" + (endPos - 1);
+        }
+      }
+      else
+      {
+        // startLine-endLine if it spans multiple lines
+        characterRange = startLine + "-" + endLine;
+      }
+      buffer.append(characterRange);
+      if (i != parseInfos.length - 1)
+      {
+        buffer.append(", ");
+      }
+    }
+    if (parseInfos.length == 1)
+    {
+      System.err.println(message + ": " + buffer);
+    }
+    else
+    {
+      System.err.println(message + "\nAt: " + buffer);
     }
   }
 
